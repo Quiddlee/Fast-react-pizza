765b3a25f8b8ccc073ad5f32d1558cd647e18837
Bogdan <thegarrybit@gmail.com>
2023-09-25 21:04:48 +0300

    feat: configure app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Cart from './features/cart/Cart.jsx';
+import Menu from './features/menu/Menu.jsx';
+import CreateOrder from './features/order/CreateOrder.jsx';
+import OrderItem from './features/order/OrderItem.jsx';
+import Home from './ui/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />,
+  },
+  {
+    path: '/order/:orderId',
+    element: <OrderItem />,
+  },
+]);
+
 function App() {
-  return <div>Hello Vite!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;

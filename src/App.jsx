72cd8b3dfb45b7a142cc106671ca8d3b9dd93498
Bogdan <thegarrybit@gmail.com>
2023-09-25 21:17:13 +0300

    feat: configure app routes with app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import React from 'react';
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Cart from './features/cart/Cart.jsx';
 import Menu from './features/menu/Menu.jsx';
 import CreateOrder from './features/order/CreateOrder.jsx';
 import OrderItem from './features/order/OrderItem.jsx';
+import AppLayout from './ui/AppLayout.jsx';
 import Home from './ui/Home.jsx';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/menu',
-    element: <Menu />,
-  },
-  {
-    path: '/cart',
-    element: <Cart />,
-  },
-  {
-    path: '/order/new',
-    element: <CreateOrder />,
-  },
-  {
-    path: '/order/:orderId',
-    element: <OrderItem />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:orderId',
+        element: <OrderItem />,
+      },
+    ],
   },
 ]);
 

240970597675ebd89643306ecbf7050ee2ccb5a6
Bogdan <thegarrybit@gmail.com>
2023-09-26 14:35:12 +0300

    feat: implement create new order

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,8 +1,14 @@
 // https://uibakery.io/regex-library/phone-number
+import { Form, redirect } from 'react-router-dom';
+
+import { createOrder } from '../../services/apiRestaurant.js';
+
+/*
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str,
   );
+ */
 
 const fakeCart = [
   {
@@ -14,7 +20,7 @@ const fakeCart = [
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: 'Vegetable',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
@@ -34,45 +40,70 @@ function CreateOrder() {
 
   return (
     <div>
-      <h2>Ready to order? Let's go!</h2>
+      <h2>Ready to order? Let&apos;s go!</h2>
 
-      <form>
+      <Form method="POST">
         <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required />
+          <label htmlFor="customer">
+            First Name
+            <input type="text" name="customer" required />
+          </label>
         </div>
 
         <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required />
-          </div>
+          <label htmlFor="phone">
+            Phone number
+            <div>
+              <input type="tel" name="phone" required />
+            </div>
+          </label>
         </div>
 
         <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required />
-          </div>
+          <label htmlFor="address">
+            Address
+            <div>
+              <input type="text" name="address" required />
+            </div>
+          </label>
         </div>
 
         <div>
-          <input
-            type="checkbox"
-            name="priority"
-            id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
-          />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority">
+            <input
+              type="checkbox"
+              name="priority"
+              id="priority"
+              // value={withPriority}
+              // onChange={(e) => setWithPriority(e.target.checked)}
+            />
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
-          <button>Order now</button>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button type="submit">Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === 'on',
+  };
+
+  const newOrder = await createOrder(order);
+
+  return redirect(`/order/${newOrder.id}/`);
 }
 
 export default CreateOrder;

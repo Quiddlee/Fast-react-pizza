dcb716429c7ad879f2b8defe1941cf42a855155d
Bogdan <thegarrybit@gmail.com>
2023-09-27 22:37:43 +0300

    feat: add interaction styles

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -90,7 +90,10 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting} type="submit">
+          <button
+            className="rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-70 disabled:hover:bg-yellow-400"
+            disabled={isSubmitting}
+            type="submit">
             {isSubmitting ? 'Placing order...' : 'Order now'}
           </button>
         </div>

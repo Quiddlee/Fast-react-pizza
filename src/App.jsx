486ac51902f3107b703228422ac122212e334dc7
Bogdan <thegarrybit@gmail.com>
2023-10-02 09:17:43 +0300

    refactor: change basename path

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,44 @@ import { action as updateOrderAction } from './features/order/UpdateOrder.jsx';
 import AppLayout from './ui/AppLayout.jsx';
 import Error from './ui/Error.jsx';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <AppLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: '/',
+          lazy: () => import('./ui/Home.jsx'),
+        },
+        {
+          path: '/menu',
+          lazy: () => import('./features/menu/Menu.jsx'),
+          loader: menuLoader,
+          errorElement: <Error />,
+        },
+        {
+          path: '/cart',
+          lazy: () => import('./features/cart/Cart.jsx'),
+        },
+        {
+          path: '/order/new',
+          lazy: () => import('./features/order/CreateOrder.jsx'),
+        },
+        {
+          path: '/order/:orderId',
+          lazy: () => import('./features/order/Order.jsx'),
+          loader: orderLoader,
+          errorElement: <Error />,
+          action: updateOrderAction,
+        },
+      ],
+    },
+  ],
   {
-    element: <AppLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: '/',
-        lazy: () => import('./ui/Home.jsx'),
-      },
-      {
-        path: '/menu',
-        lazy: () => import('./features/menu/Menu.jsx'),
-        loader: menuLoader,
-        errorElement: <Error />,
-      },
-      {
-        path: '/cart',
-        lazy: () => import('./features/cart/Cart.jsx'),
-      },
-      {
-        path: '/order/new',
-        lazy: () => import('./features/order/CreateOrder.jsx'),
-      },
-      {
-        path: '/order/:orderId',
-        lazy: () => import('./features/order/Order.jsx'),
-        loader: orderLoader,
-        errorElement: <Error />,
-        action: updateOrderAction,
-      },
-    ],
+    basename: '/Fast-react-pizza',
   },
-]);
+);
 
 function App() {
   return <RouterProvider router={router} />;
